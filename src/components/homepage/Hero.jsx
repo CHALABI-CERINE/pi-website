a5c45e696b12e7c7c9855b35fb5c6762ac745f93
CHALABI-CERINE <chalabicerinemaria@gmail.com>
2026-02-13 23:10:33 +0100

    pics

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -7,7 +7,7 @@ const HERO_IMAGES = [
   '/assets/hero/hero-2.jpg',
   '/assets/hero/hero-3.jpg',
   '/assets/hero/hero-4.jpg',
-  '/assets/hero/hero-5.jpg',
+  '/assets/hero/hero-6.jpg',
 ];
 
 export const Hero = () => {
